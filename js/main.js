b51d3b76e3c2a1a15367a59f277b63d90ca9471a
jackrieger <jack@jackrieger.com>
2019-09-14 01:37:41 -0400

    adds quick fix for modal scrolling

--- js/main.js
+++ js/main.js
@@ -5,27 +5,35 @@ const Site = {};
 Site.setModal = function () {
   const modal = document.querySelector('.info_modal');
   const button = document.querySelector('.info_button');
+  const projects = document.querySelector('.projects_container');
   let state = false;
 
-  function show() {
-    modal.classList.remove('hide');
-    modal.classList.add('show');
-    button.innerText = 'Close';
-    state = true;
+  function show(el) {
+    el.classList.remove('hide');
+    el.classList.add('show');
   }
 
-  function hide() {
-    modal.classList.remove('show');
-    modal.classList.add('hide');
-    button.innerHTML = 'About&hairsp;/&hairsp;Contact';
-    state = false;
+  function hide(el) {
+    el.classList.remove('show');
+    el.classList.add('hide');
+  }
+
+  function scrollToTop() {
+    window.scrollTo(0, 0);
   }
 
   function toggleModal() {
     if (state === false) {
-      show();
+      show(modal);
+      hide(projects);
+      scrollToTop();
+      button.innerHTML = 'Close';
+      state = true;
     } else {
-      hide();
+      show(projects);
+      hide(modal);
+      button.innerHTML = 'About&hairsp;/&hairsp;Contact';
+      state = false;
     }
   }
 
