7266d5e0208469b212adfd5289f68283aca8b1d7
Jack Rieger <jack@jackrieger.com>
2023-10-31 16:26:03 -0400

    Update lectures index

--- teaching/core-1-interaction/deploy.sh
+++ teaching/core-1-interaction/deploy.sh
@@ -1,15 +1,45 @@
-# Step 1: Navigate to the second repository (Jekyll site) and build it
-cd /path/to/second-website
-jekyll build
+# Step 1: Navigate to the Jekyll site and build it
+# cd /path/to/second-website # not necessary if run inside root folder
+bundle exec jekyll build
+if [ $? -ne 0 ]; then
+  echo "Jekyll build failed."
+  exit 1
+fi
 
 # Step 2: Copy the '_site' directory to the target folder in the first website's repository
-cp -r _site/* /path/to/first-website/target-folder/
+cp -r _site/* /Users/jack/Documents/website/jackrieger.com/teaching/core-1-interaction
+if [ $? -ne 0 ]; then
+  echo "Copy operation failed."
+  exit 1
+fi
 
-# Step 3: Navigate to the first repository, commit, and push the changes
-cd /path/to/first-website
+# Step 3: Navigate to the main repository, commit, and push the changes
+cd /Users/jack/Documents/website/jackrieger.com/teaching/core-1-interaction
+if [ $? -ne 0 ]; then
+  echo "Navigation to main repository failed."
+  exit 1
+fi
+
 git add .
-git commit -m "Updated Jekyll site content."
+if [ $? -ne 0 ]; then
+  echo "Git add operation failed."
+  exit 1
+fi
+
+# Prompt for commit message
+read -p "Enter commit message: " commitMessage
+
+git commit -m "$commitMessage"
+if [ $? -ne 0 ]; then
+  echo "Git commit failed."
+  exit 1
+fi
+
 git push
+if [ $? -ne 0 ]; then
+  echo "Git push failed."
+  exit 1
+fi
 
 echo "Deployment complete!"
 
